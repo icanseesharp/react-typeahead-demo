06e751a5c5eeb3417fd74d6daa6629625bc6fca3
Sagar Yerva <sagar.yerva@digitaslbi.com>
2019-01-10 16:46:07 +0530

    updated logo image source

--- app/scripts/components/search.js
+++ app/scripts/components/search.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-const TMDBLogo = 'https://www.themoviedb.org/assets/static_cache/27b65cb40d26f78354a4ac5abf87b2be/images/v4/logos/powered-by-rectangle-green.svg';
+const TMDBLogo = 'https://www.themoviedb.org/assets/1/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg';
 
 class SearchBox extends Component {
 
