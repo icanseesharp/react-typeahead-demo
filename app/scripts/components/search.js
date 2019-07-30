fba8a3b009ce92ad7a761df328251d777ce33d10
Sagar Yerva <sagar.yerva@digitas.com>
2019-07-30 17:02:24 +0530

    Updated logo URL and added a missing es6-promis package

--- app/scripts/components/search.js
+++ app/scripts/components/search.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-const TMDBLogo = 'https://www.themoviedb.org/assets/1/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg';
+const TMDBLogo = 'https://www.themoviedb.org/assets/2/v4/logos/powered-by-rectangle-green-dcada16968ed648d5eb3b36bbcfdd8cdf804f723dcca775c8f2bf4cea025aad6.svg';
 
 class SearchBox extends Component {
 
